dce1019ca79ec8163517f2e59d206c73bc8e0af9
jasonjayo <jasonjayo2002@gmail.com>
2023-03-22 04:38:06 +0000

    basic recipe cards working

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QGuiApplication>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    //w.resize(1920, 1080);
+    //w.resize(QGuiApplication::primaryScreen()->availableGeometry().size() * 1);
+    w.setWindowState(Qt::WindowMaximized);
     w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,9 @@
 #include <QJsonValue>
 #include <QLabel>
 #include <QTextEdit>
+#include <QGuiApplication>
+#include <QScreen>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,8 +20,15 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QPushButton* btn = new QPushButton("A button");
-    ui->gridLayout->addWidget(btn);
+
+
+    //resize(QGuiApplication::primaryScreen()->availableGeometry().size() * 1);
+    //qDebug() << QGuiApplication::primaryScreen()->availableGeometry().size();
+
+    //QPushButton* btn = new QPushButton("A button");
+    //ui->gridLayout->addWidget(btn);
+    //ui->centralwidget->setMinimumWidth(1200);
+
 
     // TODO: remove absolute file name
     QFile recipesFile("C:/Users/jason/Desktop/College/Modules/CS4076/Project/recipes/recipes.json");
@@ -31,13 +41,73 @@ MainWindow::MainWindow(QWidget *parent)
     QTextFormat* titleFormat = new QTextFormat();
     titleFormat->setProperty(QTextFormat::FontPixelSize, 12);
 
+    ui->gridLayout->setColumnMinimumWidth(0, 50);
+
+
+
+    int i = 0;
     // for each recipe
     for (const QJsonValue &recipe : recipes) {
-        //qDebug() << recipe["title"].toString();
-        btn->setText(recipe["title"].toString());
+        QVBoxLayout* recipeContainer = new QVBoxLayout();
+
+        QLabel* image = new QLabel();
+
+        QJsonArray photos = recipe["photos"].toArray();
+        QPixmap pix("C:/Users/jason/Desktop/College/Modules/CS4076/Project/recipes/" + photos.first().toString());
+        //image->setPixmap(pix.scaled( image->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        image->setPixmap(pix.scaled(300, 200));
+        //image->setScaledContents(true);
+        recipeContainer->addWidget(image);
+
         QLabel* title = new QLabel(recipe["title"].toString());
-        ui->gridLayout->addWidget(title);
-        title->setStyleSheet("QLabel {font-size: 20px}");
+        title->setStyleSheet("QLabel {font-size: 26px; background: #fff; font-weight:bold; padding: 6px;}");
+        title->setWordWrap(true);
+        recipeContainer->addWidget(title);
+
+        // stats
+        QGridLayout* statsGrid = new QGridLayout;
+        QJsonObject stats = recipe["stats"].toObject();
+        QJsonObject stats_time = stats["times"].toObject();
+
+
+        qDebug() << stats_time.value("prep").toDouble();
+
+        QString str(QString::number(stats_time.value("prep").toDouble()));
+
+
+        QLabel* timeIconLabel = new QLabel;
+        QLabel* timeValue = new QLabel(QString("Prep in " + str + " minutes"));
+        QPixmap timeIcon(":/images/icon_clock.svg");
+        timeValue->setStyleSheet("background: #fff;");
+        timeIconLabel->setPixmap(timeIcon);
+        statsGrid->addWidget(timeIconLabel, 0, 0);
+        statsGrid->addWidget(timeValue, 0, 1, 1, 4);
+        recipeContainer->addLayout(statsGrid);
+
+        QLabel* servingCountIconLabel = new QLabel;
+        QLabel* servingCountValue = new QLabel("Serves 4");
+        QPixmap servingCountIcon(":/images/icon_restaurant.svg");
+        servingCountValue->setStyleSheet("background: #fff;");
+        servingCountIconLabel->setPixmap(servingCountIcon);
+        statsGrid->addWidget(servingCountIconLabel, 1, 0);
+        statsGrid->addWidget(servingCountValue, 1, 1, 1, 4);
+        recipeContainer->addLayout(statsGrid);
+
+        QLabel* description = new QLabel(recipe["description"].toString());
+        description->setWordWrap(true);
+        description->setStyleSheet("background:#fff; padding: 6px;border: 4px solid #cfcfcf");
+        recipeContainer->addWidget(description);
+
+        QPushButton* viewBtn = new QPushButton;
+        viewBtn->setText("View recipe");
+        viewBtn->setStyleSheet("QPushButton { background: #cfcfcf; border: none; padding: 8px 0; font-weight:bold; text-transform:uppercase; font-size: 12px; }QPushButton:hover {background: #fff}");
+        recipeContainer->addWidget(viewBtn);
+
+
+
+        ui->gridLayout->addLayout(recipeContainer, 0, i);
+        i++;
+
 
     }
 
